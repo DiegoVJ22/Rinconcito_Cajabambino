2450c9087bb18e28aab5087be45d4a32aa2d6a19
DiegoVJ22 <136212395+DiegoVJ22@users.noreply.github.com>
2023-08-09 22:20:01 -0500

    Commit Inicial

---
+++ js/funciones.js
@@ -0,0 +1,376 @@
+
+// const languages = {
+//     es: {
+//         "principal":{
+//             "slogan1": "¡Abre las puertas al mundo digital",
+//             "slogan2": "con Code",
+//         },
+    
+//         "beneficios":{
+//             "b1":"✔️ Aprendizaje práctico a través de proyectos reales",
+//             "b2":"✔️ Flexibilidad en los cursos para adaptarse a las necesidades de los estudiantes",
+//             "b3":"✔️ Herramientas de última generación para aprender y experimentar con tecnologías avanzadas",
+//             "b4":"✔️ Comunidad en línea de programadores que brindan apoyo y comparten conocimientos",
+//             "b5":"✔️ Oportunidades laborales a través de la conexión con empresas de tecnología"
+//         },
+    
+    
+//         "c1":{
+//             "nombre":"Mentoría",
+//             "descripcion":"CodeLab cuenta con un programa de mentoría que brinda a los estudiantes la oportunidad de conectarse con mentores experimentados en la industria de la tecnología."
+//         },
+//         "c2":{
+//             "nombre":"Apoyo integral",
+//             "descripcion":"CodeLab brinda a los estudiantes apoyo integral, lo que significa que no solo reciben atención y orientación en su aprendizaje, sino que también tienen acceso a recursos para ayudarlos en su carrera profesional."
+//         },
+//         "c3":{
+//             "nombre":"Certificación reconocida",
+//             "descripcion":"CodeLab ofrece certificaciones reconocidas en la industria de la tecnología, lo que permite a los estudiantes demostrar sus habilidades a empleadores potenciales y avanzar en sus carreras."
+//         },
+
+            
+//         "t1":{
+//             "test":"\"CodeLab me ha brindado una educación de alta calidad en el campo de la tecnología. Los instructores son extremadamente profesionales y las herramientas de aprendizaje son excepcionales.\"",
+//             "persona":"Juan Muñoz"
+//         },
+//         "t2":{
+//             "test":"\"Me encanta la comunidad en línea de CodeLab. Es maravilloso poder conectarme con otros programadores y compartir conocimientos y experiencias. ¡Me siento motivado y emocionado por lo que está por venir gracias a CodeLab!\"",
+//             "persona":"Ana Perez"
+//         },
+//         "t3":{
+//             "test":"\"Estoy muy impresionado con la atención al cliente de CodeLab. Siempre están dispuestos a ayudar y brindan un excelente soporte técnico. Me siento muy bien apoyado mientras continúo mi educación en CodeLab.\"",
+//             "persona":"Marcos Gonzales"
+//         },
+//         "t4":{
+//             "test":"\"Antes de unirme a CodeLab, tenía muy poca experiencia en programación. Ahora, después de completar varios cursos, siento que tengo las habilidades necesarias para trabajar en el campo de la tecnología.\"",
+//             "persona":"Susana Flores"
+//         },
+    
+//         "formulario":{
+//             "titulo": "Únete a Code",
+//             // "ph-nombre":"Ingrese su Nombre",
+//             // "ph-apellido":"Ingrese su Apellido",
+//             // "ph-email":"Ingrese su E-mail",
+//             // "boton":"Unirse"
+//         },
+    
+//         "footer":{
+//             "texto":"©2023. Todos los derechos reservados."
+//         }
+//     },
+//     en: {
+//         "principal":{
+//             "slogan1": "Open the doors to the digital world",
+//             "slogan2": "with Code" 
+//         },
+    
+//         "beneficios":{
+//             "b1":"✔️ Hands-on learning through real projects",
+//             "b2":"✔️ Flexibility in courses to adapt to student needs",
+//             "b3":"✔️ State-of-the-art tools for learning and experimenting with advanced technologies",
+//             "b4":"✔️ Online community of programmers providing support and sharing knowledge",
+//             "b5":"✔️ Job opportunities through connections with technology companies"
+//         },
+
+//         "c1":{
+//             "nombre":"Mentoring",
+//             "descripcion":"CodeLab has a mentoring program that gives students the opportunity to connect with experienced mentors in the technology industry."
+//         },
+//         "c2":{
+//             "nombre":"Comprehensive support",
+//             "descripcion":"CodeLab provides students with comprehensive support, which means that not only do they receive attention and guidance in their learning, but they also have access to resources to help them in their professional careers."
+//         },
+//         "c3":{
+//             "nombre":"Recognized certification",
+//             "descripcion":"CodeLab offers recognized certifications in the technology industry, allowing students to demonstrate their skills to potential employers and advance their careers."
+//         },
+
+//         "t1":{
+//             "test":"\"CodeLab has provided me with a high quality education in the field of technology. The instructors are extremely professional and the learning tools are exceptional.\"",
+//             "persona":"Juan Muñoz"
+//         },
+//         "t2":{
+//             "test":"\"I love the CodeLab online community. It's wonderful to be able to connect with other programmers and share knowledge and experiences - I'm motivated and excited for what's to come thanks to CodeLab!\"",
+//             "persona":"Ana Perez"
+//         },
+//         "t3":{
+//             "test":"\"I am very impressed with CodeLab's customer service. They are always willing to help and provide excellent technical support. I feel very well supported as I continue my education at CodeLab.\"",
+//             "persona":"Marcos Gonzales"
+//         },
+//         "t4":{
+//             "test":"\"Before joining CodeLab, I had very little programming experience. Now, after completing several courses, I feel I have the skills necessary to work in the technology field.\"",
+//             "persona":"Susana Flores"
+//         },
+
+//         "formulario":{
+//             "titulo": "Join Code",
+//             // "ph-nombre":"Enter your Name",
+//             // "ph-apellido":"Enter your Last Name",
+//             // "ph-email":"Enter your E-mail",
+//             // "boton":"Join"
+//         },
+    
+//         "footer":{
+//             "texto":"©2023. All rights reserved."
+//         }
+//     },
+//     de: {
+//         "principal":{
+//             "slogan1": "Öffnen Sie die Türen zur digitalen Welt",
+//             "slogan2" : "mit Code"
+//         },
+    
+//         "beneficios":{
+//             "b1":"✔️ Praktisches Lernen durch reale Projekte",
+//             "b2":"✔️ Flexibilität der Kurse zur Anpassung an die Bedürfnisse der Studierenden ",
+//             "b3":"✔️ Modernste Werkzeuge zum Lernen und Experimentieren mit fortschrittlichen Technologien",
+//             "b4":"✔️ Online-Community von Programmierern, die Unterstützung bieten und Wissen austauschen",
+//             "b5":"✔️ Beschäftigungsmöglichkeiten durch Verbindungen zu Technologieunternehmen"
+//         },
+    
+//         "c1":{
+//             "nombre":"Mentoring",
+//             "descripcion":"CodeLab bietet ein Mentorenprogramm an, das Studenten die Möglichkeit gibt, mit erfahrenen Mentoren aus der Technologiebranche in Kontakt zu treten."
+//         },
+//         "c2":{
+//             "nombre":"Umfassende Unterstützung",
+//             "descripcion":"CodeLab bietet den Studierenden eine umfassende Unterstützung, d. h. sie erhalten nicht nur Aufmerksamkeit und Anleitung beim Lernen, sondern haben auch Zugang zu Ressourcen, die ihnen in ihrer beruflichen Laufbahn helfen."
+//         },
+//         "c3":{
+//             "nombre":"Anerkannte Zertifizierung",
+//             "descripcion":"CodeLab bietet anerkannte Zertifizierungen in der Technologiebranche an, mit denen die Schüler ihre Fähigkeiten gegenüber potenziellen Arbeitgebern nachweisen und ihre Karriere vorantreiben können."
+//         },
+
+//         "t1":{
+//             "test":"\"CodeLab hat mir eine qualitativ hochwertige Ausbildung im Bereich der Technologie geboten. Die Dozenten sind äußerst professionell und die Lernmittel sind außergewöhnlich.\"",
+//             "persona":"Juan Muñoz"
+//         },
+//         "t2":{
+//             "test":"\"Ich liebe die CodeLab Online-Community. Es ist wunderbar, sich mit anderen Programmierern zu vernetzen und Wissen und Erfahrungen auszutauschen - ich bin motiviert und gespannt auf das, was dank CodeLab auf mich zukommt!\"",
+//             "persona":"Ana Perez"
+//         },
+//         "t3":{
+//             "test":"\"Ich bin sehr beeindruckt vom Kundenservice von CodeLab. Sie sind immer hilfsbereit und bieten hervorragenden technischen Support. Ich fühle mich sehr gut unterstützt, während ich meine Ausbildung bei CodeLab fortsetze.\"",
+//             "persona":"Marcos Gonzales"
+//         },
+//         "t4":{
+//             "test":"\"Bevor ich zu CodeLab kam, hatte ich nur sehr wenig Programmiererfahrung. Jetzt, nachdem ich mehrere Kurse absolviert habe, habe ich das Gefühl, dass ich die notwendigen Fähigkeiten habe, um im Technologiebereich zu arbeiten.\"",
+//             "persona":"Susana Flores"
+//         },
+
+//         "formulario":{
+//             "titulo": "Mach mit bei Code",
+//             // "ph-nombre":"Geben Sie Ihren Namen ein",
+//             // "ph-apellido":"Geben Sie Ihren Nachnamen ein",
+//             // "ph-email":"Geben Sie Ihre E-Mail ein",
+//             // "boton":"Beitreten"
+//         },
+    
+//         "footer":{
+//             "texto":"©2023. Alle Rechte vorbehalten."
+//         }
+//     },
+//     fr: {
+//         "principal":{
+//             "slogan1": "Ouvrez les portes du monde numérique",
+//             "slogan2":"avec Code"
+//         },
+    
+//         "beneficios":{
+//             "b1":"✔️ Apprentissage pratique par le biais de projets réels",
+//             "b2":"✔️ Flexibilité des cours pour répondre aux besoins des étudiants",
+//             "b3":"✔️ Des outils de pointe pour apprendre et expérimenter les technologies avancées",
+//             "b4":"✔️ Communauté en ligne de programmeurs apportant leur soutien et partageant leurs connaissances",
+//             "b5":"✔️ Possibilités d'emploi grâce à des contacts avec des entreprises technologiques"
+//         },
+
+//         "c1":{
+//             "nombre":"Mentorat",
+//             "descripcion":"CodeLab dispose d'un programme de mentorat qui donne aux étudiants la possibilité d'entrer en contact avec des mentors expérimentés dans l'industrie technologique."
+//         },
+//         "c2":{
+//             "nombre":"Un soutien complet",
+//             "descripcion":"CodeLab offre aux étudiants un soutien complet, ce qui signifie qu'ils reçoivent non seulement de l'attention et des conseils dans leur apprentissage, mais qu'ils ont également accès à des ressources qui les aideront dans leur carrière professionnelle."
+//         },
+//         "c3":{
+//             "nombre":"Certification reconnue",
+//             "descripcion":"CodeLab propose des certifications reconnues dans l'industrie technologique, permettant aux étudiants de démontrer leurs compétences à des employeurs potentiels et de faire progresser leur carrière."
+//         },
+
+//         "t1":{
+//             "test":"\"CodeLab m'a fourni une éducation de haute qualité dans le domaine de la technologie. Les instructeurs sont extrêmement professionnels et les outils d'apprentissage sont exceptionnels.\"",
+//             "persona":"Juan Muñoz"
+//         },
+//         "t2":{
+//             "test":"\"J'adore la communauté en ligne de CodeLab. C'est merveilleux de pouvoir se connecter avec d'autres programmeurs et de partager des connaissances et des expériences - je suis motivée et excitée par ce qui m'attend grâce à CodeLab!\"",
+//             "persona":"Ana Perez"
+//         },
+//         "t3":{
+//             "test":"\"Je suis très impressionné par le service clientèle de CodeLab. Ils sont toujours prêts à aider et fournissent un excellent support technique. Je me sens très bien soutenu alors que je poursuis ma formation à CodeLab!\"",
+//             "persona":"Marcos Gonzales"
+//         },
+//         "t4":{
+//             "test":"\"Avant de rejoindre le CodeLab, j'avais très peu d'expérience en matière de programmation. Aujourd'hui, après avoir suivi plusieurs cours, je pense avoir les compétences nécessaires pour travailler dans le domaine de la technologie.\"",
+//             "persona":"Susana Flores"
+//         },
+    
+//         "formulario":{
+//             "titulo": "Rejoignez Code",
+//             // "ph-nombre":"Saisissez votre nom",
+//             // "ph-apellido":"Saisissez votre nom de famille",
+//             // "ph-email":"Saisissez votre e-mail",
+//             // "boton":"Rejoindre"
+//         },
+    
+//         "footer":{
+//             "texto":"©2023. Tous droits réservés."
+//         }
+//     }
+//   };
+
+// const idiomas = document.getElementById("lang-menu");
+// const textsToChange = document.querySelectorAll("[data-section]");
+
+// const changeLanguage = (language) => {
+//     const texts = languages[language];
+//     for(const textToChange of textsToChange){
+//         const section = textToChange.dataset.section;
+//         const value = textToChange.dataset.value;
+
+//         textToChange.innerHTML=texts[section][value];
+//     }
+// };
+  
+
+// idiomas.addEventListener('click', (e) => {
+//     changeLanguage(e.target.parentElement.dataset.language);
+//     cambiarPlaceHolderForm(e.target.parentElement.dataset.language);   
+// });
+
+// function toggleDarkMode() {
+//     document.body.classList.toggle("dark-mode");
+// }
+
+// function cambiarPlaceHolderForm(lang){
+//     const nombresInput = document.getElementById("nombres");
+//     const apellidosInput = document.getElementById("apellidos");
+//     const correoInput = document.getElementById("correo");
+//     const boton = document.getElementById("boton");
+
+//     if(lang=="es"){
+//         nombresInput.placeholder = "Ingrese su Nombre";
+//         apellidosInput.placeholder = "Ingrese su Apellido";
+//         correoInput.placeholder = "Ingrese su E-mail";
+//         boton.value = "Unirse";
+//     }if(lang=="en"){
+//         nombresInput.placeholder = "Enter your Name";
+//         apellidosInput.placeholder = "Enter your Last Name";
+//         correoInput.placeholder = "Enter your E-mail";
+//         boton.value = "Join";
+//     }if(lang=="de"){
+//         nombresInput.placeholder = "Geben Sie Ihren Namen ein";
+//         apellidosInput.placeholder = "Geben Sie Ihren Nachnamen ein";
+//         correoInput.placeholder = "Geben Sie Ihre E-Mail ein";
+//         boton.value = "Beitreten";
+//     }if(lang=="fr"){
+//         nombresInput.placeholder = "Saisissez votre nom";
+//         apellidosInput.placeholder = "Saisissez votre nom de famille";
+//         correoInput.placeholder = "Saisissez votre e-mail";
+//         boton.value = "Rejoindre";
+//     }
+// }
+
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      btnLeft2 = document.querySelector(".btn-left2"),
+      btnRight2 = document.querySelector(".btn-right2"),
+      slider = document.querySelector("#slider"),
+      slider2 = document.querySelector("#slider2"),
+      sliderSection = document.querySelectorAll(".slider-section-plato"),
+      sliderSection2 = document.querySelectorAll(".slider-section-cliente");
+
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+btnLeft2.addEventListener("click", e => moveToLeft2())
+btnRight2.addEventListener("click", e => moveToRight2())
+
+setInterval(() => {
+    moveToRight2()
+}, 5000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+
+let operacion2 = 0,
+    counter2 = 0,
+    widthImg2 = 100 / sliderSection2.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}
+
+function moveToRight2() {
+    if (counter2 >= sliderSection2.length-1) {
+        counter2 = 0;
+        operacion2 = 0;
+        slider2.style.transform = `translate(-${operacion2}%)`;
+        slider2.style.transition = "none";
+        return;
+    } 
+    counter2++;
+    operacion2 = operacion2 + widthImg2;
+    slider2.style.transform = `translate(-${operacion2}%)`;
+    slider2.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft2() {
+    counter2--;
+    if (counter2 < 0 ) {
+        counter2 = sliderSection2.length-1;
+        operacion2 = widthImg2 * (sliderSection2.length-1)
+        slider2.style.transform = `translate(-${operacion2}%)`;
+        slider2.style.transition = "none";
+        return;
+    } 
+    operacion2 = operacion2 - widthImg2;
+    slider2.style.transform = `translate(-${operacion2}%)`;
+    slider2.style.transition = "all ease .6s"
+    
+    
+}   
+  
+  
+
+
